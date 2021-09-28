d0ae3150aecdbd36efa790035ba3eceb8dd4278d
DrMario64 <77297488+DrMario64@users.noreply.github.com>
2021-09-28 02:38:08 -0400

    added JSON fetch request

--- swe-432-hw2-DrMario64/app.js
+++ swe-432-hw2-DrMario64/app.js
@@ -1,8 +1,20 @@
-const express = require('express')
+import fetch from "node-fetch"
+import express from "express"
+//const fetch = require('node-fetch')
+//const express = require('express')
 const app = express()
 const port = 3000
 
-const cities = ["Fairfax", "Vienna", "Falls Church", "Arlington"];
+fetch('https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0')
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => console.error(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+/*const cities = ["Fairfax", "Vienna", "Falls Church", "Arlington"];
 
 const populations = [24019, 16489, 14128, 236842];
 
@@ -16,6 +28,6 @@ app.get('/cities', (req, res) => {
 
 app.get('/populations', (req, res) => {
   return res.json(populations)
-})
+})*/
 
-module.exports = app;
+//module.exports = app;
