b93ee73642b8d91c91788c6b86a284dfd8ba7053
DrMario64 <77297488+DrMario64@users.noreply.github.com>
2021-09-28 02:45:00 -0400

    test 2

--- swe-432-hw2-DrMario64/app.js
+++ swe-432-hw2-DrMario64/app.js
@@ -1,7 +1,7 @@
-import fetch from "node-fetch"
-import express from "express"
+//import fetch from "node-fetch"
+//import express from "express"
 //const fetch = require('node-fetch')
-//const express = require('express')
+const express = require('express')
 const app = express()
 const port = 3000
 
