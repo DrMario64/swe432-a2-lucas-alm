49e37b507e39297c11dfb8444505cec60517c70f
DrMario64 <77297488+DrMario64@users.noreply.github.com>
2021-09-28 02:43:11 -0400

    test

--- swe-432-hw2-DrMario64/app.js
+++ swe-432-hw2-DrMario64/app.js
@@ -5,16 +5,16 @@ import express from "express"
 const app = express()
 const port = 3000
 
-fetch('https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0')
+/*fetch('https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0')
       .then(response => response.json())
       .then(data => console.log(data))
       .catch(err => console.error(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World')
-})
+})*/
 
-/*const cities = ["Fairfax", "Vienna", "Falls Church", "Arlington"];
+const cities = ["Fairfax", "Vienna", "Falls Church", "Arlington"];
 
 const populations = [24019, 16489, 14128, 236842];
 
@@ -28,6 +28,6 @@ app.get('/cities', (req, res) => {
 
 app.get('/populations', (req, res) => {
   return res.json(populations)
-})*/
+})
 
 module.exports = app;
